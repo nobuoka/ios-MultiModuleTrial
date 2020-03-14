efda0ecd7fcd085b0797192972c4288adfa463b1
Nobuoka Yuya <nobuoka@vividcode.info>
2020-03-15 01:54:17 +0900

    Create `FrameworkSample` project

---
+++ FrameworkSample/FrameworkSample/FrameworkSample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkSample.h
+//  FrameworkSample
+//
+//  Created by 信岡　裕也 on 2020/03/15.
+//  Copyright © 2020 Nobuoka Yuya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkSample.
+FOUNDATION_EXPORT double FrameworkSampleVersionNumber;
+
+//! Project version string for FrameworkSample.
+FOUNDATION_EXPORT const unsigned char FrameworkSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkSample/PublicHeader.h>
+
+
